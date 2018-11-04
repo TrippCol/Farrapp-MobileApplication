8897347b7a47dcdb2e72d3c0592a034244855f36
nixperful <nicolasosorioarias@hotmail.com>
2018-11-04 13:05:52 -0500

    Party View

--- app/src/main/java/escuelaing/com/co/bowmobileapp/PartyActivity.java
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/PartyActivity.java
@@ -1,26 +1,23 @@
 package escuelaing.com.co.bowmobileapp;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LogInActivity extends AppCompatActivity {
+public class PartyActivity extends AppCompatActivity {
     Button buttonBack;
 
-    Button partyRock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
+        setContentView(R.layout.activity_party);
         componentsInitialization();
         actionListenersInitialization();
     }
+
     void componentsInitialization() {
         buttonBack = (Button) findViewById((R.id.buttonBack));
-        partyRock = (Button) findViewById(R.id.eciButton);
-
     }
 
     void actionListenersInitialization() {
@@ -29,13 +26,6 @@ public class LogInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
-            }
-        });
-        partyRock.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent= new Intent(LogInActivity.this,PartyActivity.class );
-                startActivity(intent);
             }
         });
     }
