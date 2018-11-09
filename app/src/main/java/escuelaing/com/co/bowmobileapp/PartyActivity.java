23ba959b153afcf6dfefd3898fb3fe911197d3c3
nixperful <nicolasosorioarias@hotmail.com>
2018-11-09 11:18:28 -0500

    Ajustes App, ajustes para git, gitignore actualizado

--- app/src/main/java/escuelaing/com/co/bowmobileapp/PartyActivity.java
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/PartyActivity.java
@@ -1,12 +1,15 @@
 package escuelaing.com.co.bowmobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 
 public class PartyActivity extends AppCompatActivity {
     Button buttonBack;
+    Button buttonBook;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,9 @@ public class PartyActivity extends AppCompatActivity {
 
     void componentsInitialization() {
         buttonBack = (Button) findViewById((R.id.buttonBack));
+        buttonBook = (Button) findViewById((R.id.bookButton));
+        ScrollView sv = (ScrollView) findViewById(R.id.partyScrollView);
+        sv.smoothScrollTo(0,300);
     }
 
     void actionListenersInitialization() {
@@ -26,6 +32,13 @@ public class PartyActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        buttonBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(PartyActivity.this,BookActivity.class );
+                startActivity(intent);
             }
         });
     }
