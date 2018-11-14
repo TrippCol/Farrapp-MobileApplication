bb4b66edc8b05ec42cd940820f33f37714ee95ae
nixperful <nicolasosorioarias@hotmail.com>
2018-11-14 04:35:18 -0500

    Vistas finalizadas, Codigo basico insertado, rutas activas, logo en el launcher actualizado

--- app/src/main/java/escuelaing/com/co/bowmobileapp/BookActivity.java
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/BookActivity.java
@@ -1,17 +1,74 @@
 package escuelaing.com.co.bowmobileapp;
 
+import android.content.Intent;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class BookActivity extends AppCompatActivity {
+
+
+    Button bookButton, buttonBack, buttonValidate;
+    LinearLayout table;
+    EditText friend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+        componentsInitialization();
+        actionListenersInitialization();
     }
 
 
+    void componentsInitialization(){
+        buttonBack = (Button) findViewById((R.id.buttonBack2));
+        bookButton = (Button) findViewById((R.id.bookButton));
+        buttonValidate=(Button) findViewById(R.id.buttonValidate);
+
+        table=(LinearLayout) findViewById(R.id.guestTable);
+
+        friend=(EditText) findViewById(R.id.editTextFriends);
+
+    }
+    void actionListenersInitialization() {
+        final BookActivity self=this;
+        buttonValidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView titleView = new TextView(self);
+                LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                titleView.setLayoutParams(lparams);
+                titleView.setText(friend.getText());
+                titleView.setTextColor(self.getResources().getColor(R.color.colorAccent));
+                friend.setText("");
+                table.addView(titleView);
+            }
+        }
+        );
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(BookActivity.this, ConfirmationActivity.class );
+                startActivity(intent);
+            }
+        });
+    }
 
 
 }
---
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/ConfirmationActivity.java
@@ -0,0 +1,36 @@
+package escuelaing.com.co.bowmobileapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ConfirmationActivity extends AppCompatActivity {
+    Button continueButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation);
+        componentsInitialization();
+        actionListenersInitialization();
+    }
+
+
+    void componentsInitialization(){
+
+        continueButton = (Button) findViewById((R.id.continueButton));
+
+    }
+    void actionListenersInitialization() {
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(ConfirmationActivity.this, PartyListActivity.class );
+                startActivity(intent);
+            }
+        });
+
+    }
+}
