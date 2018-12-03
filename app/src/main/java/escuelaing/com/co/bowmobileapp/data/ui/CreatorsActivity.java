d0864d5c0b152264425e2ef94bb7fded08894e42
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2018-12-03 18:41:56 -0500

    added creator menu

---
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/data/ui/CreatorsActivity.java
@@ -0,0 +1,49 @@
+package escuelaing.com.co.bowmobileapp.data.ui;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.List;
+
+import escuelaing.com.co.bowmobileapp.R;
+import escuelaing.com.co.bowmobileapp.data.entities.Party;
+import escuelaing.com.co.bowmobileapp.data.network.RecyclerAdapter;
+
+public class CreatorsActivity extends AppCompatActivity {
+
+    List<Party> parties;
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+    Toolbar toolBar;
+    TextView mTitle;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.my_parties:
+                    return true;
+                case R.id.new_party:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creators);
+    }
+}
