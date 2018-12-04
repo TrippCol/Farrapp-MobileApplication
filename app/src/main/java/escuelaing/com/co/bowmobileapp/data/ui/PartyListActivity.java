47e864c9634e19114bd2a90909b3d764c8b0fa42
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-12-04 15:02:26 -0500

    CardView Background added

--- app/src/main/java/escuelaing/com/co/bowmobileapp/data/ui/PartyListActivity.java
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/data/ui/PartyListActivity.java
@@ -69,7 +69,7 @@ public class PartyListActivity extends AppCompatActivity {
             @Override
             public void onItemClick(int position) {
                 Intent intent= new Intent(getApplicationContext(),PartyActivity.class );
-                Log.e("ABCD", parties.get(position).getId().toString());
+                //Log.e("ABCD", parties.get(position).getId().toString());
                 LocalStorage.setSelectedParty(parties.get(position));
                 startActivity(intent);
             }
