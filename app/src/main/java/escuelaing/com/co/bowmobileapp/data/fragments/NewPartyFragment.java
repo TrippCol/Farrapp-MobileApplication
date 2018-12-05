5d008d4e223e5ddb0f4a0670465916d7b9185ad4
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2018-12-04 23:15:26 -0500

    fragments  for creator

---
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/data/fragments/NewPartyFragment.java
@@ -0,0 +1,24 @@
+package escuelaing.com.co.bowmobileapp.data.fragments;
+
+
+import android.support.annotation.Nullable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import escuelaing.com.co.bowmobileapp.R;
+
+
+public class NewPartyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        return inflater.inflate(R.layout.fragment_new_party, null);
+    }
+}
--- app/src/main/java/escuelaing/com/co/bowmobileapp/data/ui/CreatorsActivity.java
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/data/ui/CreatorsActivity.java
@@ -3,45 +3,43 @@ package escuelaing.com.co.bowmobileapp.data.ui;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import java.util.List;
 
 import escuelaing.com.co.bowmobileapp.R;
 import escuelaing.com.co.bowmobileapp.data.entities.Party;
-import escuelaing.com.co.bowmobileapp.data.network.RecyclerAdapter;
+import escuelaing.com.co.bowmobileapp.data.fragments.CreatorPartyListFragment;
+import escuelaing.com.co.bowmobileapp.data.fragments.NewPartyFragment;
 import escuelaing.com.co.bowmobileapp.data.persistence.LocalStorage;
 
 public class CreatorsActivity extends AppCompatActivity {
 
     List<Party> parties;
 
-    RecyclerView recyclerView;
-    RecyclerView.LayoutManager layoutManager;
-    RecyclerView.Adapter adapter;
     Toolbar toolBar;
-    TextView mTitle;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
             switch (item.getItemId()) {
                 case R.id.my_parties:
-                    return true;
+                    fragment = new CreatorPartyListFragment();
+                    break;
                 case R.id.new_party:
-                    return true;
+                    fragment = new NewPartyFragment();
+                    break;
             }
-            return false;
+            return loadFragment(fragment);
         }
     };
 
@@ -49,8 +47,20 @@ public class CreatorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creators);
+        loadFragment(new CreatorPartyListFragment());
         setViewComponents();
-        setRecyclerViewComponents();
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 
     private void setViewComponents() {
@@ -58,27 +68,8 @@ public class CreatorsActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         parties = LocalStorage.getLoadedParties();
         toolBar = (Toolbar) findViewById(R.id.app_bar);
-        //mTitle = (TextView) toolBar.findViewById(R.id.toolbar_title);
         setSupportActionBar(toolBar);
-        //mTitle.setText("Fiestas");
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-    }
-
-    private void setRecyclerViewComponents() {
-        //partyRock = (Button) findViewById(R.id.partyButton);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter = new RecyclerAdapter(parties);
-        ((RecyclerAdapter) adapter).setOnClick(new RecyclerAdapter.OnItemClicked() {
-            @Override
-            public void onItemClick(int position) {
-                Intent intent = new Intent(getApplicationContext(), PartyActivity.class);
-                LocalStorage.setSelectedParty(parties.get(position));
-                startActivity(intent);
-            }
-        });
-        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -87,7 +78,6 @@ public class CreatorsActivity extends AppCompatActivity {
         inflater.inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
-
 
     //BACK ARROW
     @Override
@@ -100,6 +90,5 @@ public class CreatorsActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(menu);
     }
-
 
 }
