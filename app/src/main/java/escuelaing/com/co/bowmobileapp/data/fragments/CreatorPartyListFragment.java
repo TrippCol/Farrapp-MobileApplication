d057a67f9e20843ae10a1b2c87284d2625480836
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2018-12-06 02:06:34 -0500

    creator view

--- app/src/main/java/escuelaing/com/co/bowmobileapp/data/fragments/CreatorPartyListFragment.java
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/data/fragments/CreatorPartyListFragment.java
@@ -1,16 +1,32 @@
 package escuelaing.com.co.bowmobileapp.data.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 import escuelaing.com.co.bowmobileapp.R;
+import escuelaing.com.co.bowmobileapp.data.entities.Party;
+import escuelaing.com.co.bowmobileapp.data.network.RecyclerAdapter;
+import escuelaing.com.co.bowmobileapp.data.persistence.LocalStorage;
+import escuelaing.com.co.bowmobileapp.data.ui.PartyActivity;
 
 public class CreatorPartyListFragment extends Fragment {
+
+    List<Party> parties;
+
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -18,6 +34,29 @@ public class CreatorPartyListFragment extends Fragment {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_creator_party_list, null);
+        View view = inflater.inflate(R.layout.fragment_creator_party_list, null);
+        setRecyclerViewComponents(view);
+        return view;
     }
+
+    private void setListToAdapter() {
+        adapter = new RecyclerAdapter(parties);
+        ((RecyclerAdapter) adapter).setOnClick(new RecyclerAdapter.OnItemClicked() {
+            @Override
+            public void onItemClick(int position) {
+                //Log.e("ABCD", parties.get(position).getId().toString());
+            }
+        });
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setRecyclerViewComponents(View view) {
+        //partyRock = (Button) findViewById(R.id.partyButton);
+        parties = LocalStorage.getLoadedParties();
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        layoutManager = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        setListToAdapter();
+    }
+
 }
--- app/src/main/java/escuelaing/com/co/bowmobileapp/data/fragments/NewPartyFragment.java
+++ app/src/main/java/escuelaing/com/co/bowmobileapp/data/fragments/NewPartyFragment.java
@@ -4,14 +4,21 @@ package escuelaing.com.co.bowmobileapp.data.fragments;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import escuelaing.com.co.bowmobileapp.R;
 
 
 public class NewPartyFragment extends Fragment {
+
+    Button createButton;
+    EditText partyName, partyDescription, partySite, partyAddress, partyDate, partyHour, PartyCover;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -19,6 +26,36 @@ public class NewPartyFragment extends Fragment {
         //with the fragment you want to inflate
         //like if the class is HomeFragment it should have R.layout.home_fragment
         //if it is DashboardFragment it should have R.layout.fragment_dashboard
-        return inflater.inflate(R.layout.fragment_new_party, null);
+        View view = inflater.inflate(R.layout.fragment_new_party, null);
+        componentsInitialization(view);
+        actionListenersInitialization();
+        return view;
     }
+
+
+    void componentsInitialization(View view) {
+        partyName = (EditText) view.findViewById(R.id.partyName);
+        partyDescription = (EditText) view.findViewById(R.id.partyDesc);
+        partySite = (EditText) view.findViewById(R.id.partySite);
+        partyAddress = (EditText) view.findViewById(R.id.partyAddress);
+        partyDate = (EditText) view.findViewById(R.id.partyDate);
+        partyHour = (EditText) view.findViewById(R.id.partyHour);
+        PartyCover = (EditText) view.findViewById(R.id.partyCover);
+        createButton = (Button) view.findViewById((R.id.createButton));
+    }
+
+    void actionListenersInitialization() {
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                postParty();
+            }
+        });
+    }
+
+    void postParty() {
+        // Post the party here
+        Log.e("aaa","lol");
+    }
+
 }
